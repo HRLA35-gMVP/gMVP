2201b112be00c45fac9ba253817c292aa911c5f7
Eric Lau <ericlaucmo@gmail.com>
2020-04-21 11:04:41 -0700

    Initialize

---
+++ README.md
@@ -0,0 +1,9 @@
+# Group MVP
+
+> A browser web app that aims to motivate users into maintaining or improving their physical fitness by connecting them with friends and family.
+
+## Table of Contents
+
+1. [Usage](#Usage)
+2. [Requirements](#requirements)
+3. [Development](#development)
---
+++ server/index.js
@@ -0,0 +1,15 @@
+// Dependencies
+const express = require('express');
+const path = require('path');
+
+// Declarations
+const server = express();
+const port = 3000;
+
+// Middleware
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(express.static(path.join(__dirname, '../client/dist')));
+
+// Server actuation
+server.listen(port, () => console.log(`Working out on port: ${port}`));
