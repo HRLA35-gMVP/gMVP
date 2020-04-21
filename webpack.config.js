961c28606e35f47bdc1e2944bb5563d8f2ca555d
Eric Lau <ericlaucmo@gmail.com>
2020-04-21 11:22:42 -0700

    Adjust webpack config and related transpiler dependencies. Filled out base structure of repo. Added base React files.

--- webpack.config.js
+++ webpack.config.js
@@ -3,21 +3,24 @@ var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
+  entry: `${SRC_DIR}/root.jsx`,
   output: {
     filename: 'bundle.js',
-    path: DIST_DIR,
+    path: DIST_DIR
   },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.jsx?/,
-        include: SRC_DIR,
         loader: 'babel-loader',
-        query: {
-          presets: ['react', 'env'],
-        },
-      },
-    ],
+        test: /\.js[x]?/,
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    ]
   },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
 };
