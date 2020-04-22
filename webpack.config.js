80a0670584a4e2a68f0076410af63dc57744f9ae
Eric Lau <ericlaucmo@gmail.com>
2020-04-22 10:38:06 -0700

    Additional webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,12 @@ module.exports = {
           presets: [
             '@babel/preset-env',
             '@babel/preset-react',
-            { plugins: ['@babel/plugin-proposal-class-properties'] }
+            {
+              plugins: [
+                '@babel/plugin-proposal-class-properties',
+                '@babel/plugin-transform-runtime'
+              ]
+            }
           ]
         }
       }
