c42bbdc317f1c679d675faf2e34438eaec2e1f40
Eric Lau <ericlaucmo@gmail.com>
2020-04-24 22:41:58 -0700

    Adding Evelyn's components to merge

---
+++ client/src/components/userPage/ChallengeStatus.jsx
@@ -0,0 +1,112 @@
+import React from 'react';
+
+import {
+  ThemeProvider,
+  CSSReset,
+  Box,
+  Flex,
+  IconButton,
+  Avatar,
+  Text,
+  Stack,
+  Input,
+  Divider,
+  Button,
+  PseudoBox,
+  Icon,
+  Grid
+} from '@chakra-ui/core';
+import customTheme from '../../theme.js';
+import ChallengeStatusList from './ChallengeStatusList.jsx';
+
+const ChallengeStatus = () => {
+  return (
+    <>
+      <ThemeProvider theme={customTheme}>
+        <CSSReset />
+        <Box
+          bg="#BEEBE9"
+          p={[2, 4, 6, 8]}
+          height="full"
+          width={[
+            '100%', // base
+            '50%', // 480px upwards
+            '25%', // 768px upwards
+            '15%' // 992px upwards
+          ]}
+        >
+          <Flex direction="column" align="center" justify="center" pt="4%">
+            <Text fontSize="3xl"> Challenge Status</Text>
+            <Box bg="#F7EEC7" w="75%" textAlign="center" pt="8px" mb="4px">
+              {' '}
+            </Box>
+            <Box bg="#F7EEC7" w="50%" textAlign="center" mt="5px" pt="5px">
+              The3Musketeers
+            </Box>
+            <Box bg="#F7EEC7" w="50%" textAlign="center" mt="5px" pt="3px">
+              PieBakeOff
+            </Box>
+            <Text
+              bg="#BEEBE9"
+              fontSize="xl"
+              w="90%"
+              textAlign="center"
+              mt="10px"
+              pt="3px"
+            >
+              Longest Streaks!
+            </Text>
+          </Flex>
+          <Grid
+            templateColumns="1fr 2fr 1fr"
+            gap={6}
+            pt="20px"
+            pl="5%"
+            pr="5%"
+            gridTemplateRows="auto"
+          >
+            <Box w="100%" h="1" textAlign="center">
+              {' '}
+              Members{' '}
+              <Box bg="#F7EEC7" w="100%" textAlign="center" pt="8px">
+                {' '}
+              </Box>
+            </Box>
+            <Box w="100%" h="10" bg="#BEEBE9" />
+            <Box w="100%" h="1" textAlign="center">
+              {' '}
+              Streaks{' '}
+              <Box bg="#F7EEC7" w="100%" textAlign="center" pt="8px">
+                {' '}
+              </Box>
+            </Box>
+          </Grid>
+          <ChallengeStatusList />
+          <Flex direction="column" align="center" justify="center" mb="10%">
+            <Box
+              rounded="lg"
+              mt="8%"
+              bg="#B0DDDB"
+              w="50%"
+              textAlign="center"
+              justify="center"
+              alignItems="center"
+              boxShadow="1px 2px 2px #8B8B8B"
+            >
+              <Text pt="5px">Time Remaining:</Text>
+              <Box bg="#E8E8E8" w="35%" mb="15px" ml="auto" mr="auto">
+                3 Days
+              </Box>
+              <Text pt="10px">First Place:</Text>
+              <Box w="50%" mb="15px" ml="auto" mr="auto">
+                <Text bg="#E8E8E8">JaneDope</Text>
+              </Box>
+            </Box>
+          </Flex>
+        </Box>
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default ChallengeStatus;
---
+++ client/src/components/userPage/EditProfile.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import { ThemeProvider, CSSReset, Box, Flex, IconButton, Avatar, Text, Stack, Input, Divider, Button} from "@chakra-ui/core";
+
+
+const EditProfile = () => {
+
+    return (
+        <>
+        <ThemeProvider>
+         <CSSReset />
+            <Stack isInline bg='#FFB6BA'pl="4%" pr="4%"width={[
+                "100%", // base
+                "50%", // 480px upwards
+                "25%", // 768px upwards
+                "15%", // 992px upwards
+            ]} justifyContent="space-between"> 
+                    <Button align="center" variant="link" color="#E8E8E8" pb="4%" pt="4%">Cancel</Button>
+                    <Text variant="link" color="#fff" fontSize="18px" fontWeight="bold" pb="4%" pt="4%">Edit Profile</Text>
+                    <Button variant="link" color="#E8E8E8" pb="4%" pt="4%" >Save</Button>
+            </Stack>
+            <Box bg='#BEEBE9'  p={[2, 4, 6, 8]} height="full"  width={[
+                "100%", // base
+                "50%", // 480px upwards
+                "25%", // 768px upwards
+                "15%", // 992px upwards
+            ]}>
+            <Flex direction="column" align="center" justify="center">
+               <Box pt="10%" >
+                <Avatar size="xl" /> </Box>
+               <Text pt="2%" fontSize={["sm", "md", "lg", "xl"]} color="##373737" align="center" justify="center">user@email.com</Text>
+            </Flex>
+               <Stack spacing={.25} pt="15%" pb="5%" align="left" pl="5%" pr="5%">
+                  <Text fontSize="10px" pb="0" >Alias</Text>
+                  <Input variant="unstyled" letterSpacing="wide" lineHeight="1" placeholder="placeholder" mt="0"></Input>
+                  <Divider borderColor="#6F6F6F"></Divider>
+               </Stack>
+
+               <Stack spacing={.25} pt="8%" pb="20%" align="left" pl="5%" pr="5%">
+                  <Text fontSize="10px"  >Password</Text>
+                  <Input variant="unstyled" letterSpacing="wide" lineHeight="1" placeholder="********" mt="0"></Input>
+                  <Divider borderColor="#6F6F6F"></Divider>
+               </Stack>
+
+               <Flex direction="column" align="center" justify="center" pb="30%">
+                    <Button variantColor="red" rounded="20px"variant="solid" w="80%">Delete Account</Button>
+              </Flex>
+            </Box>
+            
+        </ThemeProvider>
+      </>
+       
+    )
+}
+
+export default EditProfile;
