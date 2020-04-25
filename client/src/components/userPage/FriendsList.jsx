04f0f34c09dca8f51f683733af6f598379f78fc8
Eric Lau <ericlaucmo@gmail.com>
2020-04-24 23:34:35 -0700

    Implemented skeleton loading for friends list

--- client/src/components/userPage/FriendsList.jsx
+++ client/src/components/userPage/FriendsList.jsx
@@ -4,22 +4,18 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 // Chakra
-import { Spinner, SimpleGrid } from '@chakra-ui/core';
+import { SimpleGrid, Skeleton } from '@chakra-ui/core';
 import { StyledButton } from '../../styledComponents/ericStyles.js';
 
 // Components
 import Friend from './Friend.jsx';
 
 class FriendsList extends Component {
-  constructor(props) {
-    super(props);
+  state = {
+    friends: []
+  };
 
-    this.state = {
-      friends: []
-    };
-  }
-
-  async componentDidMount() {
+  componentDidMount = async () => {
     const friends = [];
     for (let friend of this.props.friends) {
       if (friend !== this.props.user) {
@@ -28,12 +24,10 @@ class FriendsList extends Component {
       }
     }
     this.setState({ friends });
-  }
+  };
 
   render() {
-    if (this.state.friends.lengh === 0) {
-      return <Spinner size="xl" />;
-    } else {
+    if (this.state.friends.length + 1 === this.props.friends.length) {
       return (
         <SimpleGrid
           columns="1"
@@ -54,36 +48,26 @@ class FriendsList extends Component {
           </Link>
         </SimpleGrid>
       );
+    } else {
+      return (
+        <SimpleGrid
+          columns="1"
+          spacing="1rem"
+          paddingLeft="1rem"
+          paddingRight="1rem"
+        >
+          {this.props.friends.map((friend, index) => {
+            if (index !== this.props.friends.length - 1) {
+              return <Skeleton key={index} height="48px" />;
+            }
+          })}
+          <Link to="/profile">
+            <StyledButton>Profile</StyledButton>
+          </Link>
+        </SimpleGrid>
+      );
     }
   }
 }
 
 export default FriendsList;
-
-// {
-//   photoURL:
-//     'https://lh4.googleusercontent.com/-lwkKptiRKDw/AAA…AAAA/AAKWJJN74m7vjEVbDK6ZDz3yUHc250eKVw/photo.jpg',
-//   displayName: 'Eric Lau',
-//   friend: 'C8gVA1A8ddcaQnawSKoJMVUOTRv2'
-// },
-// {
-//   photoURL: null,
-//   displayName: 'Joshie',
-//   friend: 'ZGjTOZNOeHcu2g1yJMNp0Q9tjlY2'
-// },
-// {
-//   photoURL: null,
-//   displayName: 'Test User #1',
-//   friend: 'A3ggHPGhlYYvX0AevZ0tyy2tQEt1'
-// },
-// {
-//   photoURL: null,
-//   displayName: 'Test User #2',
-//   friend: 'KMgRuIxINMetOinCtvQm3fwxMkb2'
-// },
-// {
-//   photoURL:
-//     'https://lh3.googleusercontent.com/a-/AOh14Gi4Y-vK45bZ2bGiwIncc9_GN6DKavRdDYjJKdH_Kw',
-//   displayName: 'Eric Lau',
-//   friend: 'kQrkwatQaggxtHcWpR2Yc1tlOxT2'
-// }
