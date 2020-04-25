15991c9db64b9ac5172afb3834b6c990eec5cab4
Eric Lau <ericlaucmo@gmail.com>
2020-04-24 21:21:36 -0700

    App.jsx Merge CSS with new code

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -5,12 +5,16 @@ import { Switch, Route, Link, Redirect } from 'react-router-dom';
 
 // Chakra + Forms
 import {
+  Box,
+  Flex,
   Button,
   Popover,
   PopoverTrigger,
   PopoverHeader,
   PopoverBody,
-  PopoverArrow
+  PopoverArrow,
+  Image,
+  PseudoBox
 } from '@chakra-ui/core';
 
 // Componenets + Styles
@@ -19,6 +23,8 @@ import Register from './landingPage/Register.jsx';
 import Login from './landingPage/Login.jsx';
 import FriendsListHelper from './userPage/FriendsListHelper.jsx';
 import BuildChallenge from './BuildChallenge/BuildAChallenge.jsx';
+
+// Styles
 import {
   StyledPopoverContent,
   StyledButton
@@ -28,43 +34,131 @@ const App = () => {
   const user = useContext(UserContext);
 
   return (
-    <Switch>
-      {!!user ? (
-        <React.Fragment>
-          <Route exact path="/friends" component={FriendsListHelper} />
-          <Redirect to="/profile" />
-          <Route exact path="/profile" component={UserPage} />
-        </React.Fragment>
-      ) : (
-        <React.Fragment>
-          <Redirect to="/" />
-          <Route exact path="/">
-            <Popover>
-              <PopoverTrigger>
-                <StyledButton>Learn More</StyledButton>
-              </PopoverTrigger>
-              <StyledPopoverContent zIndex={4}>
-                <PopoverArrow />
-                <PopoverHeader>Yo.</PopoverHeader>
-                <PopoverBody>We're going public on May 1st.</PopoverBody>
-              </StyledPopoverContent>
-            </Popover>
-            <div>
-              <Link to="/login" style={{ textDecoration: 'none' }}>
-                <Button variant="solid">Login</Button>
-              </Link>
-            </div>
-            <div>
-              <Link to="/register" style={{ textDecoration: 'none' }}>
-                <Button variant="solid">Register</Button>
-              </Link>
-            </div>
-          </Route>
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
-        </React.Fragment>
-      )}
-    </Switch>
+    <Flex direction="column" align="center" justify="center">
+      <Box
+        bg="#BEEBE9"
+        p={[2, 4, 6, 8]}
+        height="100%"
+        width={[
+          '100%', // base
+          '50%', // 480px upwards
+          '25%', // 768px upwards
+          '15%' // 992px upwards
+        ]}
+      >
+        <Switch>
+          {!!user ? (
+            <React.Fragment>
+              <Route exact path="/friends" component={FriendsListHelper} />
+              <Redirect to="/profile" />
+              <Route exact path="/profile" component={UserPage} />
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              <Redirect to="/" />
+              <Route exact path="/">
+                <Flex direction="column" alignItems="center">
+                  <div>
+                    <Box>
+                      <Image
+                        rounded="full"
+                        w="275px"
+                        h="100px"
+                        src="https://mvp2020.s3-us-west-1.amazonaws.com/streak3.png"
+                        ml="auto"
+                        mr="auto"
+                        mt="10"
+                      />
+                    </Box>
+                    <Box
+                      bg="#F7EEC7"
+                      textAlign="center"
+                      w="50%"
+                      mr="auto"
+                      ml="auto"
+                      height="30px"
+                      fontSize="xlg"
+                      boxShadow="1px 2px 3px #A2A2A2"
+                      pt="2px"
+                    >
+                      Welcome!
+                    </Box>
+                    <Box
+                      bg="#F7EEC7"
+                      textAlign="center"
+                      w="40%"
+                      mr="auto"
+                      ml="auto"
+                      mt="14px"
+                      height="20px"
+                      fontSize="xs"
+                      boxShadow="1px 2px 3px #A2A2A2"
+                      pt="2px"
+                      pb="2px"
+                    >
+                      Learn More
+                    </Box>
+                    <Box mt="15%">
+                      <Image
+                        rounded="full"
+                        size="200px"
+                        h="190px"
+                        src="https://mvp2020.s3-us-west-1.amazonaws.com/bird.png"
+                        ml="auto"
+                        mr="auto"
+                        mt="10"
+                      />
+                    </Box>
+
+                    <Box>
+                      <Link to="/login" style={{ textDecoration: 'none' }}>
+                        <PseudoBox
+                          as="button"
+                          bg="#FFB6BA"
+                          mr="auto"
+                          ml="auto"
+                          mt="20%"
+                          mb="10%"
+                          rounded="20px"
+                          fontWeight="semibold"
+                          w="100%"
+                          h="40px"
+                          color="#373737"
+                          _hover={{ bg: '#FFB6BA' }}
+                          _focus={{ boxShadow: 'outline' }}
+                        >
+                          Log In
+                        </PseudoBox>
+                      </Link>
+                    </Box>
+
+                    <Box pb="80%">
+                      <Link to="/register" style={{ textDecoration: 'none' }}>
+                        <PseudoBox
+                          as="button"
+                          bg="#FFB6BA"
+                          rounded="20px"
+                          fontWeight="semibold"
+                          h="40px"
+                          w="100%"
+                          color="#373737"
+                          _hover={{ bg: '#FFB6BA' }}
+                          _focus={{ boxShadow: 'outline' }}
+                        >
+                          Register
+                        </PseudoBox>
+                      </Link>
+                    </Box>
+                  </div>
+                </Flex>
+              </Route>
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/register" component={Register} />
+            </React.Fragment>
+          )}
+        </Switch>
+      </Box>
+    </Flex>
   );
 };
 
--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 // Declarations
 const server = express();
-const port = 3000;
+const port = 3001;
 
 // Middleware
 server.use(express.json());
