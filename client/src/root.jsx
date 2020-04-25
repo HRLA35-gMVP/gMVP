c547d7bd17379ec6b87253e690c07960bc171642
Eric Lau <ericlaucmo@gmail.com>
2020-04-24 21:23:33 -0700

    Root.jsx merge CSS and new code

--- client/src/root.jsx
+++ client/src/root.jsx
@@ -6,6 +6,12 @@ import { ThemeProvider, CSSReset } from '@chakra-ui/core';
 // Components
 import App from './components/app.jsx';
 
+// Evelyn's tests
+//import EditProfile from './components/userPage/EditProfile.jsx';
+//import ForgotPassword from './components/password/ForgotPw.jsx';
+//import CheckEmail from './components/password/CheckEmail.jsx';
+//import ChallengeStatus from './components/userPage/ChallengeStatus.jsx';
+
 // Contexts
 import UsersProvider from './providers/UsersProvider.jsx';
 import FriendsProvider from './providers/FriendsProvider.jsx';
@@ -24,3 +30,9 @@ ReactDOM.render(
   </ThemeProvider>,
   document.getElementById('root')
 );
+
+// For Evelyn's testing
+// ReactDOM.render(<EditProfile />, document.getElementById('evelyn'));
+// ReactDOM.render(<ForgotPassword />, document.getElementById('evelyn'));
+// ReactDOM.render(<CheckEmail />, document.getElementById('evelyn'));
+// ReactDOM.render(<ChallengeStatus />, document.getElementById('evelyn'));
