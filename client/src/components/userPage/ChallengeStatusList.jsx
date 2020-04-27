478bbc241ee1f369adfa8b634f4d3dd337103c10
Eric Lau <ericlaucmo@gmail.com>
2020-04-26 18:35:19 -0700

    Challenge main page response CSS

--- client/src/components/userPage/ChallengeStatusList.jsx
+++ client/src/components/userPage/ChallengeStatusList.jsx
@@ -3,7 +3,7 @@ import { getChallenge, getUser } from '../../firebase';
 import React, { Component } from 'react';
 
 // Chakra
-import { Box, Text, Grid, Skeleton } from '@chakra-ui/core';
+import { Box, Text, Grid, Skeleton, SimpleGrid } from '@chakra-ui/core';
 
 const promiseGen = async (user, currentStreak) => {
   let data = await getUser(user);
@@ -16,8 +16,7 @@ class ChallengeStatusList extends Component {
     members: [
       { alias: 'JaneDope', streaks: 13, duration: 14 },
       { alias: 'EverEric', streaks: 5, duration: 14 },
-      { alias: 'EvelynCats', streaks: 10, duration: 14 },
-      { alias: 'Jaredderaj', streaks: 8, duration: 14 }
+      { alias: 'EvelynCats', streaks: 10, duration: 14 }
     ],
     memers: []
   };
@@ -42,7 +41,7 @@ class ChallengeStatusList extends Component {
     if (this.state.memers.length === 0) {
       return (
         <React.Fragment>
-          {this.state.members.map((member, i) => (
+          {this.state.members.map((doNotUse, i) => (
             <Grid
               templateColumns="1fr 2fr 1fr"
               gap={6}
@@ -70,28 +69,21 @@ class ChallengeStatusList extends Component {
       return (
         <React.Fragment>
           {this.state.memers.slice(0, 4).map((memer, i) => (
-            <Grid
-              templateColumns="1fr 2fr 1fr"
-              gap={6}
-              key={i}
-              pl="5%"
-              pr="5%"
-              pb="2%"
-            >
+            <SimpleGrid columns={3} gap={6} key={i} pl="5%" pr="5%" pb="2%">
               <Box textAlign="center">
                 <Text>{memer.displayName}</Text>
               </Box>
-              <Box w="100%" bg="blue.500" h="4" mt="5px">
+              <Box w="100%" bg="blue.500" h="4" alignSelf="center">
                 <Box
                   w={(memer.currentStreak / this.state.duration) * 100 + '%'}
                   h="4"
                   bg="red.400"
                 />
               </Box>
-              <Box textAlign="center">
+              <Box textAlign="center" alignSelf="center">
                 <Text>{`${memer.currentStreak}/${this.state.duration}`}</Text>
               </Box>
-            </Grid>
+            </SimpleGrid>
           ))}
         </React.Fragment>
       );
