3c4579af3f624ab852d78ba3ffadb2057029d0bf
Eric Lau <ericlaucmo@gmail.com>
2020-04-26 19:19:51 -0700

    Fixed main page scrolling issue

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -77,114 +77,114 @@ const App = () => {
               '25%', // 768px upwards
               '15%' // 992px upwards
             ]}
+            position="absolute"
+            top="0"
           >
             <Redirect to="/" />
             <Route exact path="/">
               <Flex direction="column" alignItems="center">
-                <div>
-                  <Box>
-                    <Image
-                      rounded="full"
-                      w="275px"
-                      h="100px"
-                      src="https://mvp2020.s3-us-west-1.amazonaws.com/streak3.png"
-                      ml="auto"
-                      mr="auto"
-                      mt="10"
-                    />
-                  </Box>
-                  <Box
-                    bg="#F7EEC7"
-                    textAlign="center"
-                    w="50%"
-                    mr="auto"
+                <Box>
+                  <Image
+                    rounded="full"
+                    w="275px"
+                    h="100px"
+                    src="https://mvp2020.s3-us-west-1.amazonaws.com/streak3.png"
                     ml="auto"
-                    height="30px"
-                    fontSize="xlg"
-                    boxShadow="1px 2px 3px #A2A2A2"
-                    pt="2px"
-                  >
-                    Welcome!
-                  </Box>
+                    mr="auto"
+                    mt="10"
+                  />
+                </Box>
+                <Box
+                  bg="#F7EEC7"
+                  textAlign="center"
+                  w="50%"
+                  mr="auto"
+                  ml="auto"
+                  height="30px"
+                  fontSize="xlg"
+                  boxShadow="1px 2px 3px #A2A2A2"
+                  pt="2px"
+                >
+                  Welcome!
+                </Box>
 
-                  <Popover>
-                    <PopoverTrigger>
-                      <StyledButton
-                        as={Box}
-                        bg="#F7EEC7"
-                        textAlign="center"
-                        w="40%"
-                        mr="auto"
-                        ml="auto"
-                        mt="14px"
-                        height="20px"
-                        fontSize="xs"
-                        boxShadow="1px 2px 3px #A2A2A2"
-                        pt="2px"
-                        pb="2px"
-                      >
-                        Learn More
-                      </StyledButton>
-                    </PopoverTrigger>
-                    <StyledPopoverContent zIndex={4}>
-                      <PopoverArrow />
-                      <PopoverHeader>Yo.</PopoverHeader>
-                      <PopoverBody>We're going public on May 1st.</PopoverBody>
-                    </StyledPopoverContent>
-                  </Popover>
-
-                  <Box mt="15%">
-                    <Image
-                      rounded="full"
-                      size="200px"
-                      h="190px"
-                      src="https://mvp2020.s3-us-west-1.amazonaws.com/bird.png"
-                      ml="auto"
+                <Popover>
+                  <PopoverTrigger>
+                    <StyledButton
+                      as={Box}
+                      bg="#F7EEC7"
+                      textAlign="center"
+                      w="40%"
                       mr="auto"
-                      mt="10"
-                    />
-                  </Box>
+                      ml="auto"
+                      mt="14px"
+                      height="20px"
+                      fontSize="xs"
+                      boxShadow="1px 2px 3px #A2A2A2"
+                      pt="2px"
+                      pb="2px"
+                    >
+                      Learn More
+                    </StyledButton>
+                  </PopoverTrigger>
+                  <StyledPopoverContent zIndex={4}>
+                    <PopoverArrow />
+                    <PopoverHeader>Yo.</PopoverHeader>
+                    <PopoverBody>We're going public on May 1st.</PopoverBody>
+                  </StyledPopoverContent>
+                </Popover>
 
-                  <Box>
-                    <Link to="/login" style={{ textDecoration: 'none' }}>
-                      <PseudoBox
-                        as="button"
-                        bg="#FFB6BA"
-                        mr="auto"
-                        ml="auto"
-                        mt="20%"
-                        mb="10%"
-                        rounded="20px"
-                        fontWeight="semibold"
-                        w="100%"
-                        h="40px"
-                        color="#373737"
-                        _hover={{ bg: '#FFB6BA' }}
-                        _focus={{ boxShadow: 'outline' }}
-                      >
-                        Log In
-                      </PseudoBox>
-                    </Link>
-                  </Box>
+                <Box mt="15%">
+                  <Image
+                    rounded="full"
+                    size="200px"
+                    h="190px"
+                    src="https://mvp2020.s3-us-west-1.amazonaws.com/bird.png"
+                    ml="auto"
+                    mr="auto"
+                    mt="10"
+                  />
+                </Box>
 
-                  <Box pb="80%">
-                    <Link to="/register" style={{ textDecoration: 'none' }}>
-                      <PseudoBox
-                        as="button"
-                        bg="#FFB6BA"
-                        rounded="20px"
-                        fontWeight="semibold"
-                        h="40px"
-                        w="100%"
-                        color="#373737"
-                        _hover={{ bg: '#FFB6BA' }}
-                        _focus={{ boxShadow: 'outline' }}
-                      >
-                        Register
-                      </PseudoBox>
-                    </Link>
-                  </Box>
-                </div>
+                <Box>
+                  <Link to="/login" style={{ textDecoration: 'none' }}>
+                    <PseudoBox
+                      as="button"
+                      bg="#FFB6BA"
+                      mr="auto"
+                      ml="auto"
+                      mt="20%"
+                      mb="10%"
+                      rounded="20px"
+                      fontWeight="semibold"
+                      w="100%"
+                      h="40px"
+                      color="#373737"
+                      _hover={{ bg: '#FFB6BA' }}
+                      _focus={{ boxShadow: 'outline' }}
+                    >
+                      Log In
+                    </PseudoBox>
+                  </Link>
+                </Box>
+
+                <Box>
+                  <Link to="/register" style={{ textDecoration: 'none' }}>
+                    <PseudoBox
+                      as="button"
+                      bg="#FFB6BA"
+                      rounded="20px"
+                      fontWeight="semibold"
+                      h="40px"
+                      w="100%"
+                      color="#373737"
+                      _hover={{ bg: '#FFB6BA' }}
+                      _focus={{ boxShadow: 'outline' }}
+                    >
+                      Register
+                    </PseudoBox>
+                  </Link>
+                </Box>
               </Flex>
             </Route>
             <Route exact path="/login" component={Login} />
